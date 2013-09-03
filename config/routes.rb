7494720708a0158e04fc322aa55ea84aa149b8c1
zywy <m.zywno@gmail.com>
2013-09-03 13:09:08 +0200

    resources avatar nested directly under resources users

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 SampleApp::Application.routes.draw do
   
   resources :users do
+    resources :avatar, only: [:new, :show, :edit, :update, :create]
     member do
       get :following, :followers
-      resources :avatar, only: [:new, :show, :edit]
-      match 'avatar/new', to: 'avatar#create', via: 'post'
     end
   end
   resources :sessions, only: [:new, :create, :destroy]
-resources :microposts, only: [:create, :destroy]
-resources :relationships, only: [:create, :destroy]
-
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root  'static_pages#home'
   match '/signin',  to: 'sessions#new',         via: 'get'
@@ -19,6 +17,7 @@ resources :relationships, only: [:create, :destroy]
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
