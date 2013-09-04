41f7a6b91139c6418a2edf5442d8e29b7b0408a3
zywy <m.zywno@gmail.com>
2013-09-04 13:14:44 +0200

    refactoring - dividing logic into two methods 1. calculating data and 2. setting Avatar attributes

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -8,24 +8,44 @@ class Avatar < ActiveRecord::Base
 	validates :activeness, presence: true
 	validates :user_id, presence: true
 
-  before_save :calculate_calories
+  before_save :calculate_and_set_calories
 
 	scope :males, where(male: true)
 
 	private
 
-    def calculate_calories
+    def calculate_and_set_calories
+      # 1. calculate needed bmr, bee etc.
+      bee, bmr, calories_needed, fat_needed, proteins_needed, carbohydrates_needed, alcohol_needed = calculate_calories(self.male, self.weight, self.height, self.age, self.activeness)
+
+      # 2. modify Avatar
+      self.bee = bee
+      self.bmr = bmr
+      self.calories_needed = calories_needed
+      self.fat_needed = fat_needed
+      self.proteins_needed = proteins_needed
+      self.carbohydrates_needed = carbohydrates_needed
+      self.alcohol_needed = alcohol_needed
+    end
+
+    # this method does not use any Avatar attributes
+    # this method does not modify Avatar
+    # it operates only on this parameters (male, weight, height, age, activeness)
+    def calculate_calories(male, weight, height, age, activeness)
+      bee = -1
       if male
-        self.bee = 88.362+(13.397 * self.weight)+(4.799 * self.height)-(5.677 * self.age)
+        bee = 88.362+(13.397 * weight)+(4.799 * height)-(5.677 * age)
       else
-        self.bee = 447.593+(9.247 * self.weight)+(3.098 * self.height)-(4.33 * self.age)
+        bee = 447.593+(9.247 * weight)+(3.098 * height)-(4.33 * age)
       end
-      self.bmr = self.bee * self.activeness
-      self.calories_needed = self.bmr.floor
-      self.fat_needed = ((self.bmr*0.25)/9).floor
-      self.proteins_needed=((self.bmr*0.25)/4).floor
-      self.carbohydrates_needed=((self.bmr*0.25)/4).floor
-      self.alcohol_needed=((self.bmr*0.25)/7).floor
+      bmr = bee * activeness
+      calories_needed = bmr.floor
+      fat_needed = ((bmr*0.25)/9).floor
+      proteins_needed=((bmr*0.25)/4).floor
+      carbohydrates_needed=((bmr*0.25)/4).floor
+      alcohol_needed=((bmr*0.25)/7).floor
+
+      [bee, bmr, calories_needed, fat_needed, proteins_needed, carbohydrates_needed, alcohol_needed]
     end
 	
 end
