22dcd25df7ad683fa586e85b8053e6387290e3ac
zywy <m.zywno@gmail.com>
2013-09-04 13:30:37 +0200

    extracted calculate_bee method to make main calculate method smaller

--- app/models/calories_calculator.rb
+++ app/models/calories_calculator.rb
@@ -2,12 +2,7 @@ class CaloriesCalculator
 
   # all params are required : male, weight, height, age, activeness
   def self.calculate_calories(params)
-    bee = -1
-    if params[:male]
-      bee = 88.362 + (13.397 * params[:weight]) + (4.799 * params[:height])-(5.677 * params[:age])
-    else
-      bee = 447.593 + (9.247 * params[:weight]) + (3.098 * params[:height])-(4.33 * params[:age])
-    end
+    bee = calculate_bee(params)
     bmr = bee * params[:activeness]
     calories_needed = bmr.floor
     fat_needed = ((bmr * 0.25) / 9).floor
@@ -24,6 +19,16 @@ class CaloriesCalculator
         carbohydrates_needed: carbohydrates_needed,
         alcohol_needed: alcohol_needed
     }
+  end
+
+  def self.calculate_bee(params)
+    bee = -1
+    if params[:male]
+      bee = 88.362 + (13.397 * params[:weight]) + (4.799 * params[:height])-(5.677 * params[:age])
+    else
+      bee = 447.593 + (9.247 * params[:weight]) + (3.098 * params[:height])-(4.33 * params[:age])
+    end
+    bee
   end
 
 end
