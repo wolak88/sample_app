2a0322a55a6d7cccb8ee10f1ceb149a7233b6c5c
zywy <m.zywno@gmail.com>
2013-09-04 13:36:57 +0200

    formatting

--- app/models/calculators/bee_calculator.rb
+++ app/models/calculators/bee_calculator.rb
@@ -3,9 +3,9 @@ class Calculators::BeeCalculator
   # params required : male, weight, height, age
   def self.calculate(params)
     if params[:male]
-      88.362 + (13.397 * params[:weight]) + (4.799 * params[:height])-(5.677 * params[:age])
+      88.362 + (13.397 * params[:weight]) + (4.799 * params[:height]) - (5.677 * params[:age])
     else
-      447.593 + (9.247 * params[:weight]) + (3.098 * params[:height])-(4.33 * params[:age])
+      447.593 + (9.247 * params[:weight]) + (3.098 * params[:height]) - (4.33 * params[:age])
     end
   end
 
