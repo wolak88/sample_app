93f6ece0a9dec882af8f6bfc32f02ff527200586
zywy <m.zywno@gmail.com>
2013-09-04 13:30:55 +0200

    calculate_bee is private so make it private

--- app/models/calories_calculator.rb
+++ app/models/calories_calculator.rb
@@ -21,14 +21,16 @@ class CaloriesCalculator
     }
   end
 
-  def self.calculate_bee(params)
-    bee = -1
-    if params[:male]
-      bee = 88.362 + (13.397 * params[:weight]) + (4.799 * params[:height])-(5.677 * params[:age])
-    else
-      bee = 447.593 + (9.247 * params[:weight]) + (3.098 * params[:height])-(4.33 * params[:age])
+  private
+
+    def self.calculate_bee(params)
+      bee = -1
+      if params[:male]
+        bee = 88.362 + (13.397 * params[:weight]) + (4.799 * params[:height])-(5.677 * params[:age])
+      else
+        bee = 447.593 + (9.247 * params[:weight]) + (3.098 * params[:height])-(4.33 * params[:age])
+      end
+      bee
     end
-    bee
-  end
 
 end
